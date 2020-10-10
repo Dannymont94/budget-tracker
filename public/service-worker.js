ef4d741fd81a355d25a874ed979414d02b1b7198
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-10-10 00:36:44 -0400

    fixed typos in cached file paths

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,14 +6,14 @@ const FILES_TO_CACHE = [
   './index.html',
   '/js/index.js',
   '/css/styles.css',
-  '/icons/icons-72x72.png',
-  '/icons/icons-96x96.png',
-  '/icons/icons-128x128.png',
-  '/icons/icons-144x144.png',
-  '/icons/icons-152x152.png',
-  '/icons/icons-192x192.png',
-  '/icons/icons-384x384.png',
-  '/icons/icons-512x512.png'
+  '/icons/icon-72x72.png',
+  '/icons/icon-96x96.png',
+  '/icons/icon-128x128.png',
+  '/icons/icon-144x144.png',
+  '/icons/icon-152x152.png',
+  '/icons/icon-192x192.png',
+  '/icons/icon-384x384.png',
+  '/icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', function(e) {
