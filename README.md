471b1de0d400ef8025c07ff826d5d4f6f76772aa
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-10-10 00:20:47 -0400

    created service-worker.js

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # budget-tracker
+
+when offline, add requests to IndexedDB
+
+when back online, push changes from IndexedDB
+
+idb.js file goes in /public/js folder
+
+Add service worker to cache files when online and display those cached files when offline
+
+manually create web manifest with the following properties:
+  
+    name
+
+    short_name
+
+    icons
+
+    theme_color
+
+    background_color
+
+    start_url
+
+    display
+
+deploy to Heroku with MongoDB Atlas
---
+++ public/service-worker.js
@@ -0,0 +1,60 @@
+const APP_PREFIX = 'BudgetTracker-';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  '/js/index.js',
+  '/css/styles.css',
+  '/icons/icons-72x72.png',
+  '/icons/icons-96x96.png',
+  '/icons/icons-128x128.png',
+  '/icons/icons-144x144.png',
+  '/icons/icons-152x152.png',
+  '/icons/icons-192x192.png',
+  '/icons/icons-384x384.png',
+  '/icons/icons-512x512.png'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Your files were pre-cached successfully!');
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME) {
+            console.log('Removing old cache data', key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('fetch request : ' + e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(request) {
+      if (request) {
+        console.log(`responding with cache : ${e.request.url}`);
+        return request;
+      } else {
+        console.log(`file is not cached, fetching : ${e.request.url}`);
+        return fetch(e.request);
+      }
+    })
+  );
+});
